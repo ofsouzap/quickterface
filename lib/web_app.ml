42ca620321b6af800d6e4c87ae184c1956b5899e
Ofsouzap <ofsouzap@gmail.com>
2025-11-24 22:44:31 +0000

    Change to program log approach

--- lib/web_app.ml
+++ lib/web_app.ml
@@ -6,20 +6,89 @@ module type S = sig
 end
 
 module Web_app_io = struct
-  module App_module = struct
-    module type S = sig
-      type t
+  module Log = struct
+    module Output_text = struct
+      type t = { element : Dom_html.element Js.t }
 
-      val make :
-        document:Dom_html.document Js.t -> main_container:#Dom.node Js.t -> t
+      let make ~document ~text =
+        let item_div = Dom_html.createDiv document in
+        item_div##.className := Js.string "quickterface__log-item";
+        let newP = (Dom_html.createP document :> Dom_html.element Js.t) in
+        newP##.innerText := Js.string text;
+        Dom.appendChild item_div newP;
+        Lwt.return { element = item_div }
     end
-  end
 
-  module Log : sig
-    include App_module.S
+    module Input_text = struct
+      type t = {
+        element : Dom_html.element Js.t;
+        text_input_field : Dom_html.inputElement Js.t;
+        text_input_field_submit_button : Dom_html.buttonElement Js.t;
+      }
 
-    val add_output_text : t -> text:string -> unit -> unit Lwt.t
-  end = struct
+      let make ~document : t =
+        let container = Dom_html.createDiv document in
+        container##.className := Js.string "quickterface__inputs-container";
+        let text_input_field =
+          Dom_html.createInput document ~_type:(Js.string "text")
+        in
+        Dom.appendChild container text_input_field;
+        let text_input_field_submit_button = Dom_html.createButton document in
+        text_input_field_submit_button##.innerText := Js.string "Submit";
+        Dom.appendChild container text_input_field_submit_button;
+        {
+          element = container;
+          text_input_field;
+          text_input_field_submit_button;
+        }
+
+      let reset_text_input_field t () =
+        t.text_input_field##.value := Js.string "";
+        Lwt.return ()
+
+      let get_text_input_field_content t () =
+        Js.to_string t.text_input_field##.value
+
+      let set_submit_button_onclick t ~handler () =
+        t.text_input_field_submit_button##.onclick
+        := Dom.handler (fun event ->
+               handler event ();
+               Js._false)
+
+      let clear_submit_button_onclick t () =
+        t.text_input_field_submit_button##.onclick
+        := Dom.handler (Fn.const Js._true)
+
+      let set_to_readonly t () =
+        t.text_input_field##.readOnly := Js._true;
+        t.text_input_field_submit_button##.disabled := Js._true;
+        Lwt.return ()
+
+      let wait_for_text_input t () =
+        let get_text () =
+          (* Wait for the user to enter their text into the input field *)
+          let%lwt () = reset_text_input_field t () in
+          let input_submit_promise, input_submit_handler = Lwt.task () in
+          let submit_handler _ () =
+            let text_input_field_context = get_text_input_field_content t () in
+            Lwt.wakeup input_submit_handler text_input_field_context
+          in
+          set_submit_button_onclick t ~handler:submit_handler ();
+          input_submit_promise
+        in
+
+        let on_input_read () =
+          (* Do cleanup once the input has been read *)
+          let%lwt () = set_to_readonly t () in
+          clear_submit_button_onclick t ();
+          Lwt.return ()
+        in
+
+        let%lwt input_text = get_text () in
+        let%lwt () = on_input_read () in
+        Lwt.return input_text
+    end
+
     type t = {
       document : Dom_html.document Js.t;
       container : Dom_html.divElement Js.t;
@@ -34,63 +103,18 @@ module Web_app_io = struct
       Dom.appendChild container log_spacer;
       { document; container }
 
-    let add_output_text t ~text () =
-      let item_div = Dom_html.createDiv t.document in
-      item_div##.className := Js.string "quickterface__log-item";
-      Dom.appendChild t.container item_div;
-      let newP = (Dom_html.createP t.document :> Dom_html.element Js.t) in
-      newP##.innerText := Js.string text;
-      Dom.appendChild item_div newP;
-      Lwt.return ()
-  end
-
-  module Inputs : sig
-    include App_module.S
-
-    val wait_for_text_input : t -> unit -> string Lwt.t
-  end = struct
-    type t = {
-      text_input_field : Dom_html.inputElement Js.t;
-      text_input_field_submit_button : Dom_html.buttonElement Js.t;
-    }
-
-    let make ~document ~main_container : t =
-      let container = Dom_html.createDiv document in
-      container##.className := Js.string "quickterface__inputs-container";
-      Dom.appendChild main_container container;
-      let text_input_field =
-        Dom_html.createInput document ~_type:(Js.string "text")
-      in
-      Dom.appendChild container text_input_field;
-      let text_input_field_submit_button = Dom_html.createButton document in
-      text_input_field_submit_button##.innerText := Js.string "Submit";
-      Dom.appendChild container text_input_field_submit_button;
-      { text_input_field; text_input_field_submit_button }
-
-    let reset_text_input_field t () =
-      t.text_input_field##.value := Js.string "";
+    let add_output_text { document; container } ~text () =
+      let%lwt element = Output_text.make ~document ~text in
+      Dom.appendChild container element.element;
       Lwt.return ()
 
-    let get_text_input_field_content t () =
-      Js.to_string t.text_input_field##.value
-
-    let wait_for_text_input t () =
-      (* TODO - have previous inputs persist so page looks like a console history *)
-      let%lwt () = reset_text_input_field t () in
-      let input_submit_promise, input_submit_handler = Lwt.task () in
-      let on_button_click _ =
-        let text_input_field_context = get_text_input_field_content t () in
-        Lwt.wakeup input_submit_handler text_input_field_context;
-        Js._true
-      in
-      t.text_input_field_submit_button##.onclick := Dom.handler on_button_click;
-      let%lwt read_text = input_submit_promise in
-      t.text_input_field_submit_button##.onclick
-      := Dom.handler (fun _ -> Js._false);
-      Lwt.return read_text
+    let read_input_text { document; container } () =
+      let input_text = Input_text.make ~document in
+      Dom.appendChild container input_text.element;
+      Input_text.wait_for_text_input input_text ()
   end
 
-  type t = { log : Log.t; inputs : Inputs.t }
+  type t = { log : Log.t }
 
   let create_stylesheet_element document =
     let style_element = Dom_html.createStyle document in
@@ -143,18 +167,16 @@ body {
     style_element
 
   let make () : t =
-    (* TODO - this code ugly. make code pretty *)
     let document = Dom_html.document in
     Dom.appendChild Dom_html.document##.head
       (create_stylesheet_element document);
     let main_container = Dom_html.createDiv document in
     main_container##.className := Js.string "quickterface__main-container";
     let log = Log.make ~document ~main_container in
-    let inputs = Inputs.make ~document ~main_container in
     Dom.appendChild document##.body main_container;
-    { log; inputs }
+    { log }
 
-  let read_text t () = Inputs.wait_for_text_input t.inputs ()
+  let read_text t () = Log.read_input_text t.log ()
   let print_text t text () = Log.add_output_text t.log ~text ()
 end
 
--- test/examples/string_concatenator/app.ml
+++ test/examples/string_concatenator/app.ml
@@ -1,9 +1,18 @@
 open! Core
 
 module App (Io : Quickterface.Io.S) = struct
+  type empty = |
+
   let main ~io () =
-    let%lwt x = Io.read_text io () in
-    let%lwt y = Io.read_text io () in
-    let%lwt () = Io.print_text io (x ^ y) () in
-    Lwt.return ()
+    let%lwt () = Io.print_text io "String Concatenator App" () in
+    let%lwt () = Io.print_text io "message1" () in
+    let%lwt () = Io.print_text io "message2" () in
+    let%lwt () = Io.print_text io "retwuogsfdouqheoifeds" () in
+    let rec run_looping () : empty Lwt.t =
+      let%lwt x = Io.read_text io () in
+      let%lwt y = Io.read_text io () in
+      let%lwt () = Io.print_text io (x ^ y) () in
+      run_looping ()
+    in
+    match%lwt run_looping () with _ -> .
 end
