aaff2aa3a9407a32515ee522b1edc48f97b91073
Ofsouzap <ofsouzap@gmail.com>
2025-11-28 18:04:50 +0000

    Create interface for HTTP client work

---
+++ test/examples/weather_app/app.ml
@@ -0,0 +1,13 @@
+open! Core
+
+module App (Io : Quickterface.Io.S) = struct
+  let main ~io () =
+    let%lwt () = Io.print_text io "Weather App" () in
+    let%lwt () = Io.print_text io "I will now fetch the weather!" () in
+    let%lwt _response, body =
+      Io.Http_client.get (Uri.of_string "http://wttr.in/?format=3")
+    in
+    let%lwt body_string = Cohttp_lwt.Body.to_string body in
+    let%lwt () = Io.print_text io body_string () in
+    Lwt.return ()
+end
--- web_app/web_app_intf.ml
+++ web_app/web_app_intf.ml
@@ -1,12 +1,17 @@
 open! Core
 open! Js_of_ocaml
 
+module Web_app_io = struct
+  include Quickterface_web_app_backend.App
+  module Http_client = Cohttp_lwt_jsoo.Client
+end
+
 module type S = sig
   val run : unit -> unit Lwt.t
 end
 
 module Make (App : Quickterface.App.S) : S = struct
-  module App = App (Quickterface_web_app_backend.App)
+  module App = App (Web_app_io)
 
   let run () =
     let io = Quickterface_web_app_backend.App.make () in
