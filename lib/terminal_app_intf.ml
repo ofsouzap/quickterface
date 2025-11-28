0d8e40c741d8290c7baf8edb3fe5406f4fcb6739
Ofsouzap <ofsouzap@gmail.com>
2025-11-28 00:18:13 +0000

    Refactor terminal IO

--- lib/terminal_app_intf.ml
+++ lib/terminal_app_intf.ml
@@ -3,12 +3,20 @@ open! Core
 module Terminal_io = struct
   type t = { in_channel : In_channel.t; out_channel : Out_channel.t }
 
-  let read_text { in_channel; out_channel = _ } () =
+  let write_output_and_flush { in_channel = _; out_channel } ~text =
+    Out_channel.output_string out_channel text;
+    Out_channel.flush out_channel;
+    Lwt.return ()
+
+  let write_output_line_and_flush t ~text =
+    write_output_and_flush t ~text:(text ^ "\n")
+
+  let read_text ({ in_channel; out_channel = _ } as t) () =
+    let%lwt () = write_output_and_flush t ~text:"> " in
     In_channel.input_line in_channel |> Option.value_exn |> Lwt.return
 
-  let print_text { in_channel = _; out_channel } text () =
-    Out_channel.output_line out_channel text;
-    Out_channel.flush out_channel;
+  let print_text t text () =
+    let%lwt () = write_output_line_and_flush t ~text in
     Lwt.return ()
 end
 
