19f48cdc305f063a21caac3812e38b7a15251957
Ofsouzap <ofsouzap@gmail.com>
2026-01-17 11:12:44 +0000

    Rename example string concatenator app

--- test/examples/string_concatenator/terminal.ml
+++ test/examples/string_concatenator/terminal.ml
@@ -1,4 +1,7 @@
 open! Core
-module App = Quickterface_terminal_app.Terminal_app_intf.Make (App.App)
+
+module App =
+  Quickterface_terminal_app.Terminal_app_intf.Make
+    (Example_string_concatenator_app.App.App)
 
 let () = Lwt_main.run (App.run ())
--- test/examples/string_concatenator/webapp.ml
+++ test/examples/string_concatenator/webapp.ml
@@ -1,4 +1,7 @@
 open! Core
-module App = Quickterface_web_app.Web_app_intf.Make (App.App)
+
+module App =
+  Quickterface_web_app.Web_app_intf.Make
+    (Example_string_concatenator_app.App.App)
 
 let () = Lwt.async App.run
