999fc91dc6dbed9858b696c1c80636199a083682
Ofsouzap <ofsouzap@gmail.com>
2026-01-30 23:30:59 +0000

    Notty_util expect tests

---
+++ test/tui_app/test_notty_utils.ml
@@ -0,0 +1,52 @@
+open! Core
+open Notty
+open Notty.I
+open Quickterface_tui_app
+
+let no_attr = A.empty
+let img1 = string no_attr "Hello, World!"
+let img2 = (string no_attr) "top" <-> (string no_attr) "bottom"
+let img3 = (string no_attr) "left" <|> void 2 0 <|> (string no_attr) "right"
+
+let%expect_test "img1" =
+  Notty_unix.output_image img1;
+  [%expect {| Hello, World! |}]
+
+let%expect_test "img2" =
+  Notty_unix.output_image img2;
+  [%expect {|
+    top
+    bottom |}]
+
+let%expect_test "img3" =
+  Notty_unix.output_image img3;
+  [%expect {| left  right |}]
+
+let%expect_test "default box img1" =
+  let boxed_img1 = Notty_utils.boxed img1 in
+  Notty_unix.output_image boxed_img1;
+  [%expect
+    {|
+    ┌─────────────┐
+    │Hello, World!│
+    └─────────────┘
+    |}]
+
+let%expect_test "default box img2" =
+  let boxed_img2 = Notty_utils.boxed img2 in
+  Notty_unix.output_image boxed_img2;
+  [%expect {|
+    ┌──────┐
+    │top   │
+    │bottom│
+    └──────┘
+    |}]
+
+let%expect_test "default box img3" =
+  let boxed_img3 = Notty_utils.boxed img3 in
+  Notty_unix.output_image boxed_img3;
+  [%expect {|
+    ┌───────────┐
+    │left  right│
+    └───────────┘
+    |}]
