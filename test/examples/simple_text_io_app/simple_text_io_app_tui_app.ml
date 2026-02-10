7ed2d5350c855d43ce1f40967bff4b8f6cff09fd
Ofsouzap <ofsouzap@gmail.com>
2026-02-10 16:22:24 +0000

    Remove TUI apps from examples

--- test/examples/simple_text_io_app/simple_text_io_app_tui_app.ml
+++
@@ -1,4 +0,0 @@
-module Tui_app =
-  Quickterface_tui_app.Tui_app_intf.Make (Simple_text_io_app_app.App)
-
-let () = Lwt_main.run (Tui_app.run ())
--- test/examples/string_concatenator/string_concatenator_tui_app.ml
+++
@@ -1,4 +0,0 @@
-module Tui_app =
-  Quickterface_tui_app.Tui_app_intf.Make (String_concatenator_app.App)
-
-let () = Lwt_main.run (Tui_app.run ())
