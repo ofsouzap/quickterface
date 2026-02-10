79637bf56ed732ef9ee8317a0b9e669f4f3634c6
Ofsouzap <ofsouzap@gmail.com>
2026-02-10 17:26:43 +0000

    Use Cmdliner for terminal apps

--- test/examples/string_concatenator/string_concatenator_terminal_app.ml
+++ test/examples/string_concatenator/string_concatenator_terminal_app.ml
@@ -1,4 +1,4 @@
 module Terminal_app =
   Quickterface_terminal_app.Terminal_app_intf.Make (String_concatenator_app.App)
 
-let () = Lwt_main.run (Terminal_app.run ())
+let () = Terminal_app.command ~argv:Sys.argv ()
