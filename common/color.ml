3e4112f0b42178f8e5d953afcee635b0525faa81
Paul D'Souza <ofsouzap@gmail.com>
2026-02-10 16:27:24 +0000

    Remove ANSI colors from minimal terminal interface (#14)

--- common/color.ml
+++ common/color.ml
@@ -5,7 +5,6 @@ type t = Red | Green | Blue
 let red = Red
 let green = Green
 let blue = Blue
-let ansi_color_code = function Red -> 31 | Green -> 32 | Blue -> 34
 
 let css_color_string color =
   match color with Red -> "red" | Green -> "green" | Blue -> "blue"
--- common/color.mli
+++ common/color.mli
@@ -5,6 +5,5 @@ type t
 val green : t
 val red : t
 val blue : t
-val ansi_color_code : t -> int
 val css_color_string : t -> string
 val to_rgb : t -> int * int * int
